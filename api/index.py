6d95b26ffe027b6472725571d4a6ec0836ef843b
yunsin0408 <yunsin.hung@gmail.com>
2026-04-21 22:53:27 +0800

    feat: initialize database and seed default pantry categories on app startup

--- api/index.py
+++ api/index.py
@@ -6,5 +6,16 @@ sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 from app import create_app
 from config import ProductionConfig
 
-# Always use ProductionConfig on Vercel 
+# Always use ProductionConfig on Vercel
 app = create_app(config_class=ProductionConfig)
+
+with app.app_context():
+    from app.models import db, PantryCategory
+    db.create_all()
+    if not PantryCategory.query.first():
+        default_categories = [
+            "Carbohydrates", "Vegetables", "Fruits",
+            "Dairy", "Protein", "Seasoning", "Other",
+        ]
+        db.session.add_all(PantryCategory(name=n) for n in default_categories)
+        db.session.commit()
